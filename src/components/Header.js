0c6475d634f5b3e54d3b2840ac275054ecac8822
Peter <peter@MacBook-Air-de-Peter.local>
2022-03-23 19:28:41 +0100

    finished

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <nav>
+        <ul>
+          <NavLink
+            to="/"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>Acceuil</li>
+          </NavLink>
+          <NavLink
+            to="coup-de-coeur"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>Coup de Coeur</li>
+          </NavLink>
+        </ul>
+      </nav>
+      <h1>React Video</h1>
+    </div>
+  );
+};
+export default Header;
